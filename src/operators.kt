c1601db5a65554a7c4f7ee39a5b40974b1e26306
Yousef Emad <yousefemadkhazbak@gmail.com>
2026-08-10 18:35:34 +0300

    Kotlin basics

---
+++ src/operators.kt
@@ -0,0 +1,5 @@
+fun main() {
+    var name = "Youssef"
+    println(name.length)
+    println(name.random())
+}
---
+++ src/quiz1.kt
@@ -0,0 +1,25 @@
+//fun main() {
+//    var birthYear  = 2004
+//    var cityName  = "Tanta"
+//    var likesKotlin  = true
+//
+//    println("I was born in $birthYear")
+//    println("I lives in Tanta city $cityName")
+//}
+
+//fun main() {
+//    var firstName = "Youssef"
+//    var lastName = " Emad"
+//
+//    println(firstName + lastName)
+//    println("$firstName$lastName")
+//}
+
+fun main() {
+    var age = 22
+    var height = 1.73
+    var result = age + height
+    println(age)
+    println(height)
+    println(result)
+}
